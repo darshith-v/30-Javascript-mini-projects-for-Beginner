859b7e72ff6788a0deef0c1e75b6fa0a6a68a881
darshithv <darshithv3392@gmail.com>
2024-01-28 18:25:21 +0530

    completed QR-Code generator project

--- QR-code-generator-app/script.js
+++ QR-code-generator-app/script.js
@@ -1,5 +1,23 @@
-let 
+let qrinput = document.querySelector('.qr-input');
+let imgBox = document.querySelector('.imgBox');
+let qrImage = document.querySelector('.qrImage');
 
-function textToQRcode() {
-  
+
+function generateQR() {
+
+  if (qrinput.value.length > 0){
+      qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrinput.value;
+    
+      imgBox.classList.add('show-img');
+  } else{
+    qrinput.classList.add('error');
+    setTimeout(() => {
+      qrinput.classList.add('error');
+    }, 1000);
+  }
+
 }
+
+document.querySelector('.qr-button').addEventListener('click', () => { generateQR() });
+
+
